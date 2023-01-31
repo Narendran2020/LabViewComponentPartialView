ed4468313844124fa689ee082c8a7fe2d5e44eba
Narendran2020 <72653314+Narendran2020@users.noreply.github.com>
2023-01-31 12:45:29 +0530

    initialCommit

---
+++ LabViewComponentPartialView/Components/CommentViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using LabViewComponentPartialView.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LabViewComponentPartialView.Components
+{
+    public class CommentViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(int id)
+        {
+            HttpClient client = new HttpClient();
+            ICollection<CommentViewModel> comments =
+           client.GetFromJsonAsync<ICollection<CommentViewModel>>
+           ("https://jsonplaceholder.typicode.com/comments?postId=" + id).Result;
+            return View("~/views/components/comment.cshtml", comments);
+        }
+    }
+}
---
+++ LabViewComponentPartialView/Controllers/ArticleController.cs
@@ -0,0 +1,22 @@
+﻿using LabViewComponentPartialView.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LabViewComponentPartialView.Controllers
+{
+    public class ArticleController : Controller
+    {
+        public IActionResult Index()
+        {
+            PostViewModel model = new PostViewModel
+            {
+                Id = 1,
+                Title = "What is ASP.NET Core",
+                Author = "Shailendra Chauhan",
+                Body = "ASP.NET Core is an open-source framework for building UI and  Api"
+
+            };
+            return View(model);
+            
+        }
+    }
+}
